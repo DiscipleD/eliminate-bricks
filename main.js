192cc3c6ce0706e467f1db9a08b4c7f618e24e7d
DiscipleD <jianzhe.ding@shuyun.com>
2016-03-11 18:21:20 +0800

    init project structure

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * @author jianzhe.ding
+ * @homepage https://github.com/discipled/
+ * @since 2016-03-11 16:58
+ */
+
+(function (window) {
+	var canvas = document.getElementById('main-screen');
+	var ctx = canvas.getContext('2d');
+	var animation;
+})(window);
